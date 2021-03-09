ab88618dc63c0bcca16b9c20852ee4cd9641a4dc
Yakin Nikita <yakin95@gmail.com>
2021-03-09 12:28:47 +0500

    react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-	"extends": [
-		"./rules/common.json",
-		"./rules/typescript.json",
-		"./rules/vue.json"
+	'extends': [
+		'./rules/common.json',
+		'./rules/typescript.json',
+		'./rules/vue.json'
 	]
-}
+};
